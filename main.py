3422a1c0487c31e87804e9a999e45254f9bf8a97
ThomasYiCui <93477247+ThomasYiCui@users.noreply.github.com>
2022-03-02 19:12:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode([600, 600])
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+    screen.fill((255, 0, 0))
+    pygame.display.update()
+pygame.quit()
