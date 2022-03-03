1e600b25c7163dc827a418bc35c33a0c3355422d
ThomasYiCui <93477247+ThomasYiCui@users.noreply.github.com>
2022-03-02 19:13:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ pygame.init()
 
 screen = pygame.display.set_mode([600, 600])
 
+class player():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    def draw(self):
+        pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y, 20, 20))
+
+p = player()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
     screen.fill((255, 0, 0))
+    p.draw()
     pygame.display.update()
 pygame.quit()
